829770095c0efd37dc1904639121dec01ae71b0c
Whelpley <whelpley@gmail.com>
2017-03-13 15:36:01 -0700

    New component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: '<div><app-simple-form></app-simple-form></div>'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'I am ready to DO THIS!';
+  title = 'I am ready to LEARN ANGULAR!';
 }
