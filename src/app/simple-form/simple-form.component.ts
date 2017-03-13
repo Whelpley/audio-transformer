a6d28e876026271aa387d628fbbe2df8b086bd02
Whelpley <whelpley@gmail.com>
2017-03-13 15:53:41 -0700

    Create a simple form

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
-  template: `
-    <p>
-      simple-form Works!
-    </p>
-  `,
+  template: `<div>
+    <input #myInput type="text">
+    <button (click)="onClick(myInput.value)">Click this every day!</button>
+    </div>`,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
+  onClick(value){
+    console.log(value);
+  }
   constructor() { }
 
   ngOnInit() {
