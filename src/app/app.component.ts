7dcf11a92954993ec972ad6d73336f6efdf5b536
Whelpley <whelpley@gmail.com>
2017-03-21 11:41:55 -0700

    Trashing old version, adding new audio components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 // import { MailService } from './mail.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'I am ready to LEARN ANGULAR!';
+// export class AppComponent {
+//   title = 'I am ready to LEARN ANGULAR!';
 
-  onUpdate(id, text){
-    this.mail.update(id, text);
-  }
+//   onUpdate(id, text){
+//     this.mail.update(id, text);
+//   }
 
-  constructor(
-      @Inject('mail') private mail
-  ){}
+//   constructor(
+//       @Inject('mail') private mail
+//   ){}
+// }
+
+export class AppComponent  implements OnInit {
+    private audioContext: AudioContext;
+    private loadingSample: boolean = false;
+    private audioBuffer: AudioBuffer;
+
+    ngOnInit() {
+        this.audioContext = new AudioContext();
+    }
+    
+    fetchSample(): Promise<AudioBuffer> {
+        return fetch('samples/snare.wav')
+            .then(response => response.arrayBuffer())
+            .then(buffer => {
+                return new Promise((resolve, reject) => {
+                    this.audioContext.decodeAudioData(
+                        buffer,
+                        resolve,
+                        reject
+                    );
+                })
+            });
+    }
 }
